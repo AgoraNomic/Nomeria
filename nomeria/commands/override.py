f5d95e35e438c914bbf4bd2e94276369d8b539c3
Aris (Karim) Merchant <wizkarim@gmail.com>
2018-07-16 20:16:29 -0700

    Fix command documentation

--- nomeria/commands/override.py
+++ nomeria/commands/override.py
@@ -2,8 +2,28 @@ from evennia.commands.default import general, help
 
 
 class CmdGet(general.CmdGet):
+    """
+    Pick something up
+
+    Usage:
+      get <obj>
+
+    Picks up an object from your location and puts it in
+    your inventory.
+    """
     aliases = ["grab", "take", "pick up"]
 
 
 class CmdHelp(help.CmdHelp):
+    """
+    View help or a list of topics
+
+    Usage:
+      help <topic or command>
+      help list
+      help all
+
+    This will search for help on commands and other
+    topics related to the game.
+    """
     aliases = ["man", "?"]
