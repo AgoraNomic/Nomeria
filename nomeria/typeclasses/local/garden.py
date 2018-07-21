3b47f9301b681be0375ebfa537155988c319bcbc
Aris (Karim) Merchant <wizkarim@gmail.com>
2018-07-21 16:03:04 -0700

    Add fountain sounds

--- nomeria/typeclasses/local/garden.py
+++ nomeria/typeclasses/local/garden.py
@@ -1,3 +1,5 @@
+import random
+from evennia import TICKER_HANDLER
 from ..objects import Object
 
 
@@ -6,5 +8,24 @@ class Fountain(Object):
     Implements the town fountain.
     """
 
+    def at_object_creation(self):
+        TICKER_HANDLER.add(45, self.repeat)
+
     def repeat(self):
-        pass
+        """Called at regular intervals to provide motion."""
+        loc = self.location
+        if loc:
+            rand = random.random()
+            if rand < 0.2:
+                string = "The fountain burbles softly."
+            elif rand < 0.4:
+                string = "Light passes through the fountain, forming a rainbow."
+            elif rand < 0.5:
+                string = "Water splashes outside the fountain."
+            elif rand < 0.6:
+                string = "You feel calm and peaceful."
+            elif rand < 0.65:
+                string = "The fountain shimmers oddly. Is it glowing?"
+            else:
+                return
+        loc.msg_contents(string)
