da5db30c7d6be5c42c0627541aeec28ca52d805d
Aris (Karim) Merchant <wizkarim@gmail.com>
2018-07-21 17:08:46 -0700

    Add more sounds for the fountain

--- nomeria/typeclasses/local/garden.py
+++ nomeria/typeclasses/local/garden.py
@@ -9,7 +9,7 @@ class Fountain(Object):
     """
 
     def at_object_creation(self):
-        TICKER_HANDLER.add(45, self.repeat)
+        TICKER_HANDLER.add(60, self.repeat)
 
         self.locks.add("get:false()")
         self.db.get_err_msg = "The fountain is anchored to the ground."
@@ -19,8 +19,12 @@ class Fountain(Object):
         loc = self.location
         if loc:
             rand = random.random()
-            if rand < 0.2:
+            if rand < 0.1:
                 string = "The fountain burbles softly."
+            elif rand < 0.2:
+                string = "The wind blows through the trees and bushes."
+            elif rand < 0.3:
+                string = "Birds chirp from the tree tops above"
             elif rand < 0.4:
                 string = "Light passes through the fountain, forming a rainbow."
             elif rand < 0.5:
